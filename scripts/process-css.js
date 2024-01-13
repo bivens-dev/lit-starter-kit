fcf5df96d78e6cd134ab6963383f7f30c6ec848c
mdh <113126707+bivens-dev@users.noreply.github.com>
2024-01-13 23:42:29 +0000

    Create CSS build script

---
+++ scripts/process-css.js
@@ -0,0 +1,126 @@
+import {readdir, readFile, writeFile} from 'node:fs/promises';
+import {transform} from 'lightningcss';
+// eslint-disable-next-line node/no-extraneous-import
+import * as prettier from 'prettier';
+
+const configuration = {
+  sourcePath: '/src',
+  outputPath: '/build',
+  minify: false,
+  lit: {
+    enabled: true,
+    typescript: true,
+  },
+  native: {
+    enabled: true,
+    sourceMaps: true,
+  },
+};
+
+class StylesheetProcessor {
+  #configuration;
+
+  constructor(config) {
+    this.#configuration = config;
+  }
+
+  async start() {
+    const sourcePath = new URL(
+      `..${this.#configuration.sourcePath}`,
+      import.meta.url
+    ).pathname;
+
+    const cssFiles = await this.getSourceFiles(sourcePath);
+
+    for (const file of cssFiles) {
+      const results = await this.#processFile(file);
+      await this.#writeNativeFile(results);
+      this.#writeLitFile(results);
+    }
+  }
+
+  async getSourceFiles(directory) {
+    const cssFiles = new Set();
+
+    try {
+      const files = await readdir(directory, {recursive: true});
+      for (const file of files) {
+        if (file.endsWith('.css')) {
+          cssFiles.add(`${directory}/${file}`);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    return cssFiles;
+  }
+
+  async #processFile(fileName) {
+    const fileContents = await readFile(fileName);
+    const {code, map} = transform({
+      filename: fileName,
+      code: fileContents,
+      minify: this.#configuration.minify,
+      sourceMap: this.#configuration.native.sourceMaps,
+    });
+
+    const results = {
+      cssFile: code,
+      sourceMap: map,
+      fileName: fileName,
+    };
+
+    return results;
+  }
+
+  async #writeNativeFile(resultsObject) {
+    if (!this.#configuration.native.enabled) {
+      return;
+    }
+
+    if (this.#configuration.native.sourceMaps) {
+      const sourceMapFileName = `${resultsObject.fileName.replace(
+        this.#configuration.sourcePath,
+        `${this.#configuration.outputPath}${this.#configuration.sourcePath}`
+      )}.map`;
+
+      await writeFile(sourceMapFileName, resultsObject.sourceMap, {flag: 'w'});
+    }
+
+    const stylesheetFileName = `${resultsObject.fileName.replace(
+      this.#configuration.sourcePath,
+      `${this.#configuration.outputPath}${this.#configuration.sourcePath}`
+    )}`;
+
+    await writeFile(stylesheetFileName, resultsObject.cssFile, {flag: 'w'});
+  }
+
+  async #writeLitFile(resultsObject) {
+    if (!this.#configuration.lit.enabled) {
+      return;
+    }
+
+    const fileOutput = `import {css} from 'lit';
+        export const styles = css\`
+        ${resultsObject.cssFile}\`;
+      `;
+
+    const formattedOutput = await prettier.format(fileOutput, {
+      parser: 'typescript',
+    });
+
+    if (this.#configuration.lit.typescript) {
+      const typescriptFileName = resultsObject.fileName + '.ts';
+
+      await writeFile(typescriptFileName, formattedOutput, {flag: 'w'});
+    } else {
+      const javascriptFileName = resultsObject.fileName + '.js';
+
+      await writeFile(javascriptFileName, formattedOutput, {flag: 'w'});
+    }
+  }
+}
+
+const processor = new StylesheetProcessor(configuration);
+await processor.start();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import {html, css, LitElement} from 'lit';
+import {html, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import {styles} from './styles.css.js';
 
 @customElement('simple-greeting')
 export class SimpleGreeting extends LitElement {
-  static styles = css`
-    p {
-      color: blue;
-    }
-  `;
+  static styles = styles;
 
   @property()
   name = 'Somebody';
